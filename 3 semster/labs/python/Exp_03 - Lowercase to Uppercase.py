e51ec79ced67c5c3eb3087812fa804f2af70fe60
sravanthi nuthnapati <73333232+sravanthi153@users.noreply.github.com>
2021-11-04 14:22:50 +0530

    Create Exp_03 - Lowercase to Uppercase.py

---
+++ 3 semster/labs/python/Exp_03 - Lowercase to Uppercase.py
@@ -0,0 +1,10 @@
+#  Python Program to convert lowercase string to uppercase string
+
+# Get the input string
+# Convert lower case to upper case 
+# Print the output
+
+print(input().upper())
+
+# Input: sathyabama
+# Output: SATHYABAMA
